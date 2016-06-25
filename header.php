0c0a25e4683b6b18c35c95953205c722c272ec33
Dajana Stojanović <daja15@live.com>
2016-06-25 14:55:59 +0200

    bootstrap+početni php i css fileovi

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='css/style.css' rel='stylesheet' type='text/css'>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once "header.php"; ?>
+
+<div class="row početna">
+pripremamo se
+</div>
+
+<?php include_once "footer.php"; ?>
