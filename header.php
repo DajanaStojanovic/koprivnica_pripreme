4fe9104ab0470e12bc20d1a285a7eb6941168542
Dajana Stojanović <daja15@live.com>
2016-07-02 14:45:18 +0200

    izmjena navigacije

--- header.php
+++ header.php
@@ -40,9 +40,8 @@ session_start();?>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="index.php">NASLOVNICA</a></li>
-        <li><a href="#kontakt">PRIJAVI PROBLEM</a></li>
-        <li><a href="#">RIJEŠENI PROBLEMI</a></li>
+        <li><a href="#prijavi_problem">PRIJAVI PROBLEM</a></li>
+        <li><a href="#rijeseni_problemi">RIJEŠENI PROBLEMI</a></li>
       </ul>
 
 
