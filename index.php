737196b702c8419fed22d149ec2f884abe65befb
Dajana Stojanović <daja15@live.com>
2016-07-02 11:48:22 +0200

    index

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <?php include_once "header.php"; ?>
 
 <div class="row pocetna">
-	<div class="col-md-4 post">
-	<div class="kontent">
-		<i class="fa fa-bed" style="font-size:36px"></i>
-		<h3>Smještaj</h3>
-		<p>Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. Postao je popularan tijekom 1960-ih s pojavom Letraset listova s odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
-	</div>
-	</div>
 
-	<div class="col-md-4 post">
-		<div class="kontent">
-		<i class="fa fa-users" style="font-size:36px"></i>
-		<h3>Usluge</h3>
-		<p>Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. Postao je popularan tijekom 1960-ih s pojavom Letraset listova s odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
-	</div>
-	</div>
-
-	<div class="col-md-4 post">
-		<div class="kontent">
-		<i class="fa fa-spinner" style="font-size:36px"></i>
-		<h3>Nadolazeći događaji</h3>
-		<p>Lorem Ipsum je jednostavno probni tekst koji se koristi u tiskarskoj i slovoslagarskoj industriji. Lorem Ipsum postoji kao industrijski standard još od 16-og stoljeća, kada je nepoznati tiskar uzeo tiskarsku galiju slova i posložio ih da bi napravio knjigu s uzorkom tiska. Taj je tekst ne samo preživio pet stoljeća, već se i vinuo u svijet elektronskog slovoslagarstva, ostajući u suštini nepromijenjen. Postao je popularan tijekom 1960-ih s pojavom Letraset listova s odlomcima Lorem Ipsum-a, a u skorije vrijeme sa software-om za stolno izdavaštvo kao što je Aldus PageMaker koji također sadrži varijante Lorem Ipsum-a.</p>
-	</div>
-	</div>
 </div>
 
 
@@ -93,8 +71,18 @@
 	</div>
 </div>
 <!--kraj kontakt-->
-<<<<<<< HEAD
-<?php include_once "footer.php"; ?>
+
+
+
+
+<!--google mapa-->
+<div class="googlemapa">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44210.43858368393!2d16.786181290969715!3d46.16762169639677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476627031a318147%3A0xbe043a049b25af09!2sKoprivnica!5e0!3m2!1shr!2shr!4v1467450420289" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<!--kraj mape -->
+
+
+
 <script>
 $("#send").click(function(){
 var ime = $("#ime").val();
@@ -119,13 +107,5 @@ var email = $("#email").val();
 });
 
 </script>
-=======
-
-<!--google mapa-->
-<div class="googlemapa">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44210.43858368393!2d16.786181290969715!3d46.16762169639677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476627031a318147%3A0xbe043a049b25af09!2sKoprivnica!5e0!3m2!1shr!2shr!4v1467450420289" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
-<!--kraj mape -->
-
 <?php include_once "footer.php"; ?>
->>>>>>> origin/master
+
